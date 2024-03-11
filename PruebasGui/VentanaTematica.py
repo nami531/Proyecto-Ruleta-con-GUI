c6a4a93708703931ffa961193c0bcd7323270651
nami-teis <nadiamarquez27112005@gmail.com>
2024-03-11 11:33:21 +0100

    Ventana Boton Tematica refactorizada

--- PruebasGui/VentanaTematica.py
+++ PruebasGui/VentanaTematica.py
@@ -4,10 +4,20 @@ from Label import Label
 from Jugador import Jugador
 from Boton import Boton
 import sys
+from pygame import Surface
 
 class VentanaTematica:
 
-    vista : Vista
+    vista: Vista
+    width : int
+    height : int
+    screen : Surface
+    labels : list[Label]
+    x_botones : int
+    tamanho_botones: tuple[int,int]
+    margen :int
+    fuente : int
+    colores : dict[str, tuple[int,int,int]]
 
     def __init__(self, width, height):
         self.vista = Vista()
@@ -17,19 +27,28 @@ class VentanaTematica:
         pygame.display.set_caption("Ruleta de la suerte")
 
         margen = 150
-        self.labels = []
-        self.inputs = []
-        # self.jugadores = jugadores
         self.botones = []
 
-        # Label(100, 100, self.vista.turno(jugadores[i]), 24, (0,0,0))
-        # self.i = i 
+        self.x_botones = 100
+        self.tamanho_botones = (120, 50)
+        self.margen = 150
+
+        self.fuente = 24
+
+        self.colores = { "fondo" : (234,234,234),
+                        "negro" : (0,0,0),
+                        "blanco": (255,255,255),
+                        "morado": (204, 202, 234),
+                        "morado_hover" : (159, 149, 175),
+                        "azul" : (199, 228, 255) ,
+                        "azul_hover" : (46, 155, 255)
+        }
 
         for i in range(len(self.vista.tematicas_disponibles)): 
             if i >= len(self.vista.tematicas_disponibles) //2: 
-                self.botones.append(Boton(100 + margen * (i-2), 400, 100, 25, (12,23,43), (27,11,5), self.vista.tematicas_disponibles[i], (0,0,0), 24, i))
+                self.botones.append(Boton(100 + margen * (i-2), 300, self.tamanho_botones[0], self.tamanho_botones[1], self.colores["morado"], self.colores["morado_hover"], self.vista.tematicas_disponibles[i], self.colores["negro"], self.fuente, i))
             else: 
-                self.botones.append(Boton(100 + margen * i, 200, 100, 25, (12,23,43), (27,11,5), self.vista.tematicas_disponibles[i], (0,0,0), 24, i))
+                self.botones.append(Boton(100 + margen * i, 100, self.tamanho_botones[0], self.tamanho_botones[1], self.colores["morado"], self.colores["morado_hover"], self.vista.tematicas_disponibles[i], self.colores["negro"], self.fuente, i))
 
     def ejecutar(self)-> int | None:
         eleccion = False
@@ -47,27 +66,17 @@ class VentanaTematica:
                     if boton.fue_presionado(mouse_pos, event) and not boton.eliminado:
                         eleccion = True
                         return boton.valor
-
-                for entrada in self.inputs: 
-                    entrada.update(mouse_pos, event)
                     
             for boton in self.botones:
                 boton.update(mouse_pos) 
 
             
 
-            self.screen.fill((0, 0, 255))  # Limpiar la pantalla con color blanco
+            self.screen.fill(self.colores["fondo"])  # Limpiar la pantalla con color blanco
 
             # Dibujar elementos en la pantalla
             for boton in self.botones:
                 boton.draw(self.screen) 
-
-            for label in self.labels: 
-                label.draw(self.screen)
-
-            for entrada in self.inputs: 
-                entrada.draw(self.screen)
-
            
 
             # Actualizar la pantalla
